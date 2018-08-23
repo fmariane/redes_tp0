cf7cf768cf3830ecd24ee84ab2b4a8a981963a50
fmariane <f_mariane@hotmail.com>
2018-08-22 22:52:24 -0300

    troca da funcao encode para o cliente

--- cliente.py
+++ cliente.py
@@ -7,6 +7,18 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.bind((HOST, PORT))
 
-data = s.recv(50)
+#data = s.recv(50)
 
-print("recebi do servodor { " + data.decode('ascii')+ " }")
+def enc(str, key):
+    #works as a table to shift letters
+    sigma = "abcdefghijklmnopqrstuvwxyz"
+    aux = []
+    for i in range (0, len(str)):
+        for j in range(0,25):
+            if(str[i] == sigma[j]):
+               c = sigma[(j+key)%25]
+               aux.append(c)
+    #parse list into string
+    return ''.join(aux)
+
+#print("recebi do servodor { " + data.decode('ascii')+ " }")
--- servidor.py
+++ servidor.py
@@ -8,19 +8,4 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 data = input()
 
-#encode function
-def enc(str, key):
-    #works as a table to shift letters
-    sigma = "abcdefghijklmnopqrstuvwxyz"
-    aux = []
-    for i in range (0, len(str)):
-        for j in range(0,25):
-            if(str[i] == sigma[j]):
-               c = sigma[(j+key)%25]
-               aux.append(c)
-    #parse list into string
-    return ''.join(aux)
-
-
-encoded_data: str = enc(data, 1)
-s.sendto(encoded_data.encode('ascii'), (HOST, PORT) )
+#s.sendto(data, (HOST, PORT) )
