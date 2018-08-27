de36ae1d121487809954333e2576bff17406cc45
fmariane <f_mariane@hotmail.com>
2018-08-26 23:52:51 -0300

    servidor funcionando com threads

--- cliente.py
+++ cliente.py
@@ -1,24 +1,70 @@
 import socket
+import struct
+import sys
 
-HOST = 'localhost'
-PORT = 5454
+def parameterHandler():
+    parameter = {
+        "server_IP": sys.argv[1],
+        "server_PORT": sys.argv[2],
+        "msg": sys.argv[3],
+        "cypher_key": sys.argv[4],
+    }
+    
+    return parameter
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def ackReceipt(ack):
+    if not ack:
+        return False
+    return True
 
-s.bind((HOST, PORT))
+#======== Encription function ============
+def caesar_cypher_enc(str, key):
+   #sigma works as a table to shift letters
+   sigma = "abcdefghijklmnopqrstuvwxyz"
+   aux = []
+   for i in range (0, len(str)):
+      for j in range(0,26):
+         if(str[i] == sigma[j]):
+            c = sigma[(j+key)%26]
+            aux.append(c)
+   #parse list into string
+   return ''.join(aux)
 
-#data = s.recv(50)
+#====== Network routines
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conecta-se no servidor, 
+# que deve estar executando na mesma máquina, no porto 5000
 
-def enc(str, key):
-    #works as a table to shift letters
-    sigma = "abcdefghijklmnopqrstuvwxyz"
-    aux = []
-    for i in range (0, len(str)):
-        for j in range(0,25):
-            if(str[i] == sigma[j]):
-               c = sigma[(j+key)%25]
-               aux.append(c)
-    #parse list into string
-    return ''.join(aux)
+# gets parameters passed by command line 
+parameter = parameterHandler()
 
-#print("recebi do servodor { " + data.decode('ascii')+ " }")
+HOST = parameter["server_IP"]          # Endereco IP do servidor
+PORT = int(parameter["server_PORT"])   # Porta que o servidor está
+dest = (HOST, PORT)
+tcp.connect(dest)
+print('Conectado! Para sair, use CTRL+X\n')
+
+
+str_size = input()
+
+#send string size
+tcp.send (str_size.encode('ascii'))
+ack = tcp.recv(8)
+if not ackReceipt(ack):
+    print("FAIL TO SEND STRING SIZE!")
+   
+
+#if any ack was received, send encoded string
+ack = []
+int_key = int(parameter["cypher_key"])
+str_encoded = caesar_cypher_enc(parameter["msg"], int_key)
+tcp.send(str_encoded.encode('ascii'))
+ack = tcp.recv(8)
+if not ackReceipt(ack):
+    print("FAIL TO SEND STRING!")
+
+#send crypt string and wait for decrypt response
+tcp.send(parameter["cypher_key"].encode('ascii'))
+response = tcp.recv( int(str_size) )
+
+tcp.close()
--- servidor.py
+++ servidor.py
@@ -1,11 +1,62 @@
+# USED AS REFERENCE https://gist.github.com/Integralist/3f004c3594bbf8431c15ed6db15809ae
+
 import socket
-import time
+import struct
+import sys
+import threading
 
-HOST = 'localhost'
-PORT = 5454
+#======== Decription function
+def caesar_cypher_dec(str, key):
+    #sigma works as a table to shift letters
+    sigma = "abcdefghijklmnopqrstuvwxyz"
+    aux = []
+    for i in range (0, len(str)):
+        for j in range(0,26):
+            if(str[i] == sigma[j]):
+                c = sigma[(j-key)%26]
+                aux.append(c)
+    #join method parse list into string
+    return ''.join(aux)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#====== Network routines ==============
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = ''              # Não precisa indicar o endereço (é o da máquina local)
+PORT = 5000            # Porta que o servidor deve ficar
+orig = (HOST, PORT)
 
-data = input()
+# Deve primeiro iniciar a abertura passiva com bind e listen
+tcp.bind(orig)
+tcp.listen(10) #mantem no maximo 10 conexoes em espera
 
-#s.sendto(data, (HOST, PORT) )
+# Agora está pronto para receber uma conexão vinda de um cliente
+#con, cliente = tcp.accept()
+
+
+#client_connection defined in while True statement
+def clientThread(client_connection):
+    ack_byte = "ACK".encode('ascii')
+    
+    #receive from client 
+    msg_size = client_connection.recv(32) #4 byte integer
+    client_connection.send(ack_byte)
+    
+    received = client_connection.recv(int(msg_size))
+    client_connection.send(ack_byte)
+    
+    cypher_key = client_connection.recv(32)
+    client_connection.send(ack_byte)
+    
+    #decode msg according to caesar_cypher algo and send it back to client
+    decoded = caesar_cypher_dec(received, cypher_key)
+    client_connection.send(decoded.encode('ascii'))
+    client_connection.close()
+
+while True:
+    client_connection, address = tcp.accept()
+    print('Conectado por', address)
+    
+    client_worker_thread = threading.Thread(
+        target=clientThread,
+        args=(client_connection,)
+    )
+    client_worker_thread.start()
